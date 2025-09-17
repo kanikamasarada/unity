e65540edbb7681fc29d7f41cd22491121118ca55
HJ-AD\07045 <247045@himejo.jp>
2025-09-17 10:25:29 +0900

    変更内容

--- My project/Assets/InventoryUI.cs
+++ My project/Assets/InventoryUI.cs
@@ -3,11 +3,21 @@ using UnityEngine;
 public class InventoryUI : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    [SerializeField]private GameObject
+    [SerializeField] private GameObject inventoryPanel;
+    private bool isOpen = false;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            ToggleInventory();
+        }
     }
+    private void ToggleInventory()
+    {
+        isOpen = !isOpen;
+        inventoryPanel.SetActive(isOpen);
+    } 
 }
