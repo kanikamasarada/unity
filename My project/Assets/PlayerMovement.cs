f98e807e2a4d654d80add44d1bdda888601767ba
HJ-AD\07045 <247045@himejo.jp>
2025-09-19 14:17:22 +0900

    変更内容

--- My project/Assets/PlayerMovement.cs
+++ My project/Assets/PlayerMovement.cs
@@ -17,21 +17,21 @@ public class PlayerMove : MonoBehaviour
 
     void Update()
     {
-        // 接地判定
+        
         isGrounded = controller.isGrounded;
         if (isGrounded && velocity.y < 0)
         {
-            velocity.y = -2f; // 地面に押し付けておく
+            velocity.y = -2f; 
         }
 
-        // 入力で移動
+        
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
 
-        // 重力のみ
+        
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
     }
